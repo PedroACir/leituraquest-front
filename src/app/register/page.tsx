9049462d9f5fc28c7a855da6b202b69965ae9ce2
Pedro Cirqueira <pedro.augusto1228@gmail.com>
2024-10-25 17:03:23 -0300

    first commit

---
+++ src/app/register/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className="flex h-screen justify-center items-center">
+        <h1 className="text-4xl">
+            Página de cadastro...
+        </h1>
+    </div>
+  )
+}
+
+export default Page;
